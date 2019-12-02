1778aba5345fc4bb6569037c3f3a6878fa8c9aef
tad-okazaki <44688069+tad-okazaki@users.noreply.github.com>
2019-12-01 17:18:13 -0800

    Dirty dirty code
    There is an error in syntax when querying the database

---
+++ tads_dirty_corner.java
@@ -0,0 +1,39 @@
+package Main;
+// matt
+        import javax.xml.transform.Result;
+        import java.sql.*;
+        import java.util.Scanner;
+
+public class Main {
+    private static String DBUser = "bettagj";
+    private static String pass = "Password0*";
+    private static String url = "jdbc:mysql://leia.cs.spu.edu/bettagj_db?serverTimezone=UTC";
+
+    public static void main(String args[])
+    {
+
+        try{
+            int i = 1;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, DBUser, pass);
+            Statement stmt = con.createStatement();
+            System.out.println("Who do you want to search for?: ");
+            Scanner userInput = new Scanner(System.in);
+            String inputArtist = userInput.nextLine();
+            ResultSet rs = stmt.executeQuery("SELECT artist_name, song_name, album_name, song_duration FROM artist_song as JOIN song s ON as.song_id = s.song_id JOIN artist a ON as.artist_id = a.artist_id WHERE songName = " + "'" + inputArtist+ "'"+ ";");
+            if(rs != null) {
+                if(rs.isBeforeFirst()) {
+                    while(rs.next()) {
+                        System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
+                    }
+                }
+            }
+            else {
+                System.out.println("Could Not Be Found");
+            }
+            con.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
