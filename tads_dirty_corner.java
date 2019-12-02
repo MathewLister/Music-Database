3826e49d51790d00d89f3b546070edef9ca7a3a7
bettagj <49009810+bettagj@users.noreply.github.com>
2019-12-01 17:39:24 -0800

    changed queries

--- tads_dirty_corner.java
+++ tads_dirty_corner.java
@@ -1,8 +1,8 @@
 package Main;
-// matt
-        import javax.xml.transform.Result;
-        import java.sql.*;
-        import java.util.Scanner;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     private static String DBUser = "bettagj";
@@ -20,7 +20,8 @@ public class Main {
             System.out.println("Who do you want to search for?: ");
             Scanner userInput = new Scanner(System.in);
             String inputArtist = userInput.nextLine();
-            ResultSet rs = stmt.executeQuery("SELECT artist_name, song_name, album_name, song_duration FROM artist a, song s, artist_song sa, album ab WHERE sa.artist_id = a.artist_id AND sa.song_id = s.song_id AND ab.artist_id = a.artist_id AND s.song_name = " + "'" + inputArtist+ "'"+ ";";
+            String query = "SELECT artist_name, song_name, album_name, song_duration FROM artist a, song s, artist_song sa, album ab WHERE sa.artist_id = a.artist_id AND sa.song_id = s.song_id AND ab.artist_id = a.artist_id AND s.song_name = " + "'" + inputArtist+ "'"+ ";";
+            ResultSet rs = stmt.executeQuery(query);
             if(rs != null) {
                 if(rs.isBeforeFirst()) {
                     while(rs.next()) {
