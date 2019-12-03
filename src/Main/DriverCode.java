ddc42565b7f2cdfb4f86a29ddf3431a7554d7f7b
AngryPorkchop11 <listermathew0@gmail.com>
2019-12-02 21:38:20 -0800

    Broke master but I fixed it..

--- src/Main/DriverCode.java
+++ src/Main/DriverCode.java
@@ -4,21 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DriverCode {
-//Validates that option input is within our case range 0-...
-    public static boolean ValidOption(int choice){
-        if((choice < 0) || (choice > 9 )) {
+    //Validates that option input is within our case range 0-...
+    public static boolean ValidOption(int choice) {
+        if ((choice < 0) || (choice > 9)) {
             return false;
         }
         return true;
     }
 
-//Driver Code
+    //Driver Code
     public static void main(String args[]) throws SQLException, IOException {
         String menu = "*************MENU*************\n0. Exit\n********SEARCH BY********\n1. Song\n2. Artist\n3. Album\n4. Label\n5. Play List\n6. Concert\n7. Genre";
+        String insert = "********INSERT********\n8. Insert Song in Playlist\n9. Insert a Concert";
+        String delete = "********DELETE********\n10. Delete a Playlist\n11. Delete a Concert";
         int option = 100;
         String input;
         //This grabs options from console
@@ -29,76 +30,92 @@ public class DriverCode {
         //Loop menu until user chooses 0 for exit
         while (option != 0) {
             System.out.println(menu + "\n");
+            System.out.println(insert + "\n");
+            System.out.println(delete + "\n");
             System.out.print("Enter Menu Option: ");
-            option = in.nextInt();
-            //IDK how to make this work --> if we cant figure it out its all good
-            /*
-            ===> 1. Get input in as a string
-                2. try to convert from string to int
-                3. if conversion fails, wrong input
-                String tmp = in.nextLine();
-                try{
-                    option = Integer.parseInt(tmp);
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                    System.out.println("Wrong Input");
-                }
-
-             */
-            //This try should make sure the user only enters int for the option so the program wont crash
-           /* try {
-                option = in.nextInt();
-            }
-            catch (InputMismatchException e){
-                System.out.println("You have entered invalid data!");
-            }
-*           */
-            System.out.println();
-            //Validating that the user is entering 0-...
-            while (ValidOption(option) == false) {
-                System.out.println("Invalid input! Please try again");
-                System.out.println(menu + "\n");
-                System.out.print("Enter Menu Option: ");
-
-                option = in.nextInt();
+            //option = in.nextInt();
+            String tmp = in.nextLine(); // get input from user
+            try {
+                option = Integer.parseInt(tmp); // try to convert string to int
+            } catch (NumberFormatException e) {
+                //e.printStackTrace();
+                System.out.println("Wrong Input");
+                System.out.println("-------------------------------------------------------------------\n");
                 System.out.println();
+                continue; // skip to next loop iteration
             }
-         switch (option) {
-             //exit
-             case 0:
-                return;
-         //Search by:
-             //Song
-             case 1:
-                 System.out.print("Song name to search: ");
-                 input = reader.readLine();
-                 Query.song(input);
-                break;
-             //Artist
-             case 2:
-                 //Query.artist();
-                 break;
-             //Album
-             case 3:
-                 break;
-             //Label
-             case 4:
-                 break;
-             //Play List
-             case 5:
-                 break;
-             //Concert
-             case 6:
-                 break;
-             //Genre
-             case 7:
-                break;
-             default:
-                 System.out.println("Please enter an option 0-7");
-                 break;
-         }
-         System.out.println("-------------------------------------------------------------------\n");
-         System.out.println();
+
+            System.out.println();
+
+            if (!ValidOption(option)) { // check if option is a valid option in menu
+                System.out.println("Invalid input! Please try again");
+                System.out.println("-------------------------------------------------------------------\n");
+                System.out.println();
+                continue; // skip to next loop iteration
+            }
+            switch (option) {
+                //exit
+                case 0:
+                    return;
+                //Search by:
+                //Song
+                case 1:
+                    System.out.print("Song name to search: ");
+                    input = reader.readLine();
+                    Query.song(input);
+                    break;
+                //Artist
+                case 2:
+                    //Query.artist();
+                    break;
+                //Album
+                case 3:
+                    break;
+                //Label
+                case 4:
+                    break;
+                //Play List
+                case 5:
+                    System.out.print("Playlist name to search: ");
+                    input = reader.readLine();
+                    Query.playList(input);
+                    break;
+                //Concert
+                case 6:
+                    break;
+                //Genre
+                case 7:
+                    System.out.print("Genre name: ");
+                    input = reader.readLine();
+                    Query.genre(input);
+                    break;
+                case 8:
+                    // insert song in playlist
+                    int songID, playlistID;
+                    String output;
+                    try{
+                        System.out.print("Song ID (integer): ");
+                        input = reader.readLine();
+                        songID = Integer.parseInt(input);
+                        System.out.print("Playlist ID (integer): ");
+                        input = reader.readLine();
+                        playlistID = Integer.parseInt(input);
+                        output = Query.insertSongIntoPlaylist(songID, playlistID);
+                        System.out.println(output);
+                    } catch (Exception e) {
+                        //e.printStackTrace();
+                        System.out.println("Invalid Input");
+                    }
+                    break;
+                case 9:
+                    //insert a concert?
+                    break;
+                default:
+                    System.out.println("Please enter an option 0-9");
+                    break;
+            }
+            System.out.println("-------------------------------------------------------------------\n");
+            System.out.println();
         }
     }
 }
--- src/Main/Query.java
+++ src/Main/Query.java
@@ -2,7 +2,10 @@ package Main;
 
 import javax.xml.transform.Result;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class Query {
     public static void song (String userInput) throws SQLException {
@@ -43,9 +46,36 @@ public class Query {
 
     }
 
-    public static void playList (Scanner userInput)
+    public static void playList (String userInput)
     {
+        int duration;
+        String tmp;
+        Connection con = DatabaseConnection.getConnection();
+        String query = "SELECT p.playlist_name, SUM(s.song_duration) AS LENGTH FROM playlist_song ps JOIN song s ON ps.song_id = s.song_id JOIN playlist p ON ps.playlist_id = p.playlist_id WHERE p.playlist_name LIKE '%" + userInput + "%' GROUP BY p.playlist_id;";
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs != null) {
+                if(rs.isBeforeFirst()) {
+                    System.out.println("Playlist Name: || Duration: HH:mm:ss");
+                    while(rs.next()) {
 
+                        tmp = rs.getString(2);
+                        duration = Integer.parseInt(tmp);
+                        Date date = new Date(duration * 1000L);
+                        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+                        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+                        tmp = df.format(date);
+                        System.out.println(rs.getString(1) + " || " + tmp);
+                    }
+                }
+            } else {
+                System.out.println("Empty Set");
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void concert (Scanner userInput)
@@ -53,7 +83,69 @@ public class Query {
 
     }
 
-    public static void genre (Scanner userInput)
+    public static void genre (String userInput)
+    {
+        Connection con = DatabaseConnection.getConnection();
+        String query = "SELECT artist_name, album_name, genre, album_date, album_id FROM album JOIN artist ON album.artist_id = artist.artist_id WHERE album.genre LIKE '%" + userInput + "%';";
+        try{
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if(rs != null) {
+                if(rs.isBeforeFirst()) {
+                    System.out.println("Artist || Album || Genre || Album Date || Album ID");
+                    while(rs.next()) {
+                        System.out.println(rs.getString(1) + " || " + rs.getString(2) + " || " + rs.getString(3) + " || " + rs.getString(4) + " || " + rs.getString(5));
+                    }
+                }
+            }
+            else {
+                System.out.println("Could Not Be Found");
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String insertSongIntoPlaylist (int songID, int playlistID)
+    {
+        String returnMessage = "Could not complete insertion";
+        String checkQuery = "SELECT playlist_song_id FROM playlist_song WHERE song_id = " + songID + " AND playlist_id = " + playlistID + ";";
+        String checkSong = "SELECT * FROM song WHERE song_id = " + songID + ";";
+        String checkPlaylist = "SELECT * FROM playlist WHERE playlist_id = " + playlistID + ";";
+        String insertQuery = "INSERT INTO playlist_song (song_id, playlist_id) VALUES (" + songID + ", " + playlistID + ");";
+
+        Connection con = DatabaseConnection.getConnection();
+        try{
+            // create 3 separate statements because only one ResultSet object per Statement object can be open at the same time
+            Statement stmt1 = con.createStatement();
+            Statement stmt2 = con.createStatement();
+            Statement stmt3 = con.createStatement();
+            ResultSet rs1 = stmt1.executeQuery(checkQuery);
+            ResultSet rs2 = stmt2.executeQuery(checkSong);
+            ResultSet rs3 = stmt3.executeQuery(checkPlaylist);
+            if(!rs2.next()) { // check if song is in the song table
+                returnMessage = "Song does not exist";
+            } else if(!rs3.next()) { // check if playlist is in the playlist table
+                returnMessage = "Playlist does not exist";
+            } else if(rs1.next()) { // check if song is already related to the playlist
+                returnMessage = "Song is already in playlist";
+            } else {
+                PreparedStatement ps = con.prepareStatement(insertQuery);
+                int ret = ps.executeUpdate(); // execute query and store return value in ret
+                //System.out.println("Return from Update: " + ret);
+                if(ret == 1) {
+                    returnMessage = "Successful Insertion";
+                }
+            }
+            con.close(); // all lower objects are closed when connection is closed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnMessage;
+    }
+
+    public static void insertConcert (String userInput)
     {
 
     }
