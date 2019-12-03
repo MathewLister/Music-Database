bac7d06c00166688f108b232865540777f649a84
bettagj <bettagj@spu.edu>
2019-12-02 19:14:46 -0800

    added search album by genre

--- src/Main/DriverCode.java
+++ src/Main/DriverCode.java
@@ -8,15 +8,15 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DriverCode {
-//Validates that option input is within our case range 0-...
-    public static boolean ValidOption(int choice){
-        if((choice < 0) || (choice > 9 )) {
+    //Validates that option input is within our case range 0-...
+    public static boolean ValidOption(int choice) {
+        if ((choice < 0) || (choice > 9)) {
             return false;
         }
         return true;
     }
 
-//Driver Code
+    //Driver Code
     public static void main(String args[]) throws SQLException, IOException {
         String menu = "*************MENU*************\n0. Exit\n********SEARCH BY********\n1. Song\n2. Artist\n3. Album\n4. Label\n5. Play List\n6. Concert\n7. Genre";
         int option = 100;
@@ -32,7 +32,7 @@ public class DriverCode {
             System.out.print("Enter Menu Option: ");
             //option = in.nextInt();
             String tmp = in.nextLine(); // get input from user
-            try{
+            try {
                 option = Integer.parseInt(tmp); // try to convert string to int
             } catch (NumberFormatException e) {
                 //e.printStackTrace();
@@ -74,51 +74,54 @@ public class DriverCode {
                 option = in.nextInt();
                 System.out.println();
             }*/
-            if( !ValidOption(option) ) { // check if option is a valid option in menu
+            if (!ValidOption(option)) { // check if option is a valid option in menu
                 System.out.println("Invalid input! Please try again");
                 System.out.println("-------------------------------------------------------------------\n");
                 System.out.println();
                 continue; // skip to next loop iteration
             }
-         switch (option) {
-             //exit
-             case 0:
-                return;
-         //Search by:
-             //Song
-             case 1:
-                 System.out.print("Song name to search: ");
-                 input = reader.readLine();
-                 Query.song(input);
-                break;
-             //Artist
-             case 2:
-                 //Query.artist();
-                 break;
-             //Album
-             case 3:
-                 break;
-             //Label
-             case 4:
-                 break;
-             //Play List
-             case 5:
-                 System.out.print("Playlist name to search: ");
-                 input = reader.readLine();
-                 Query.playList(input);
-                 break;
-             //Concert
-             case 6:
-                 break;
-             //Genre
-             case 7:
-                break;
-             default:
-                 System.out.println("Please enter an option 0-7");
-                 break;
-         }
-         System.out.println("-------------------------------------------------------------------\n");
-         System.out.println();
+            switch (option) {
+                //exit
+                case 0:
+                    return;
+                //Search by:
+                //Song
+                case 1:
+                    System.out.print("Song name to search: ");
+                    input = reader.readLine();
+                    Query.song(input);
+                    break;
+                //Artist
+                case 2:
+                    //Query.artist();
+                    break;
+                //Album
+                case 3:
+                    break;
+                //Label
+                case 4:
+                    break;
+                //Play List
+                case 5:
+                    System.out.print("Playlist name to search: ");
+                    input = reader.readLine();
+                    Query.playList(input);
+                    break;
+                //Concert
+                case 6:
+                    break;
+                //Genre
+                case 7:
+                    System.out.print("Genre name: ");
+                    input = reader.readLine();
+                    Query.genre(input);
+                    break;
+                default:
+                    System.out.println("Please enter an option 0-7");
+                    break;
+            }
+            System.out.println("-------------------------------------------------------------------\n");
+            System.out.println();
         }
     }
 }
--- src/Main/Query.java
+++ src/Main/Query.java
@@ -2,8 +2,10 @@ package Main;
 
 import javax.xml.transform.Result;
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class Query {
     public static void song (String userInput) throws SQLException {
@@ -46,7 +48,7 @@ public class Query {
 
     public static void playList (String userInput)
     {
-        int hours, minutes, seconds, duration;
+        int duration;
         String tmp;
         Connection con = DatabaseConnection.getConnection();
         String query = "SELECT p.playlist_name, SUM(s.song_duration) AS LENGTH FROM playlist_song ps JOIN song s ON ps.song_id = s.song_id JOIN playlist p ON ps.playlist_id = p.playlist_id WHERE p.playlist_name LIKE '%" + userInput + "%' GROUP BY p.playlist_id;";
@@ -55,19 +57,22 @@ public class Query {
             ResultSet rs = stmt.executeQuery(query);
             if (rs != null) {
                 if(rs.isBeforeFirst()) {
-                    System.out.println("Playlist Name: || Duration: HH:MM:ss");
+                    System.out.println("Playlist Name: || Duration: HH:mm:ss");
                     while(rs.next()) {
+
                         tmp = rs.getString(2);
                         duration = Integer.parseInt(tmp);
-                        hours = duration / 3600;
-                        minutes = (duration % 3600) / 60;
-                        seconds = duration % 60;
-                        System.out.println(rs.getString(1) + " || " + hours + ":" + minutes + ":" + seconds);
+                        Date date = new Date(duration * 1000L);
+                        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+                        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+                        tmp = df.format(date);
+                        System.out.println(rs.getString(1) + " || " + tmp);
                     }
                 }
             } else {
                 System.out.println("Empty Set");
             }
+            con.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -78,8 +83,27 @@ public class Query {
 
     }
 
-    public static void genre (Scanner userInput)
+    public static void genre (String userInput)
     {
-
+        Connection con = DatabaseConnection.getConnection();
+        String query = "SELECT artist_name, album_name, genre, album_date, album_id FROM album JOIN artist ON album.artist_id = artist.artist_id WHERE album.genre LIKE '%" + userInput + "%';";
+        try{
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if(rs != null) {
+                if(rs.isBeforeFirst()) {
+                    System.out.println("Artist || Album || Genre || Album Date || Album ID");
+                    while(rs.next()) {
+                        System.out.println(rs.getString(1) + " || " + rs.getString(2) + " || " + rs.getString(3) + " || " + rs.getString(4) + " || " + rs.getString(5));
+                    }
+                }
+            }
+            else {
+                System.out.println("Could Not Be Found");
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
